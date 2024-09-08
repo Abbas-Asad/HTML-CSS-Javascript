5fc4124b6164f6886a62afe56927fcb1e28498cc
Abbas Asad <168946441+abbas-asad@users.noreply.github.com>
2024-09-09 01:59:18 +0500

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,7 +2,7 @@ let btns = document.querySelectorAll(".btn");
 let textarea = document.querySelector(".txtarea");
 btns.forEach((btn) =>
   btn.addEventListener("click", () => {
-    if (btn.textContent === "C") {
+    if (btn.textContent === "AC") {
       textarea.value = "";
     } else if (btn.textContent === "=") {
       textarea.value = eval(textarea.value);
