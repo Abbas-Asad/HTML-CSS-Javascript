d6c6f3d5712615739ccbf2e86df1a6cc31e426d6
Abbas Asad <abbas07tech@gmail.com>
2025-09-14 00:42:11 +0500

    Moved Html_Css_Js_Projects into Html_Css_Js_Projects folder

--- Random_color_generator/script.js
+++
@@ -1,7 +0,0 @@
-let btn_col_chng = document.querySelector(".btn-col-chng");
-let para = document.querySelector(".p");
-btn_col_chng.addEventListener("click", () => {
-  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  document.body.style.backgroundColor = randomColor;
-  para.textContent = randomColor;
-});
